bc140b9765e999f98b406f87a1682ced796ce06b
shahriarsajeeb <support@becodemy.com>
2026-04-06 19:59:20 +0530

    optimized error handling

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,18 +4,27 @@ type Watermark = {
     opacity?: number;
     size?: string;
 };
-type OutPutFiles = ["360p", "480p", "720p", "1080p"];
+type OutPutFiles = ("360p" | "480p" | "720p" | "1080p")[];
 
 declare class Media {
-    static convert({ apiKey, keyname, outPutBucketId, outputs, generateSubtitles, watermark, }: {
+    static convert({ apiKey, keyname, outPutBucketId, outputs, generateSubtitles, watermark, webhookUrl, }: {
         apiKey: string;
         keyname: string;
         outPutBucketId: string;
         outputs: OutPutFiles;
         generateSubtitles?: boolean;
         watermark?: Watermark;
+        webhookUrl: string;
     }): Promise<{
         trackingId: string;
+    }>;
+    static verifyWebhook({ apiKey, headers, rawBody, body, }: {
+        apiKey: string;
+        headers: Record<string, any>;
+        rawBody: string;
+        body: any;
+    }): Promise<{
+        valid: boolean;
     }>;
 }
 declare const media: typeof Media;
